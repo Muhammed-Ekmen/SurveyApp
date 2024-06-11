a73f81bb616a70146aee3cd2cac5cda869e483a3
Muhammed Semih <semihmuhammedekmen@gmail.com>
2024-06-11 14:22:05 +0300

    init Project

---
+++ SurveyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurveyApp
+//
+//  Created by kingSemih on 11.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
